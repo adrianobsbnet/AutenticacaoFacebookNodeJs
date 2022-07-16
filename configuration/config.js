32c0c45b9febc734e156ce0b6bcf6bf35081e35f
Adriano Cardoso <adrianobsbnet@gmail.com>
2022-07-16 17:46:46 -0300

    Criado autenticacao com facebook

---
+++ configuration/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  api_key:
+    "146104913115-egdii1vc1vb3v5s7vt3pqsn4mie79dgg.apps.googleusercontent.com",
+  api_secret: "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",
+  callback_url: "http://localhost:3000/auth/google/callback",
+  api_key_fb: "5348999571882889",
+  api_secret_fb: "5d8237a89b693ee27a3ba6094befd7e5",
+  callback_url_fb: "http://localhost:3000/auth/facebook/callback",
+};
